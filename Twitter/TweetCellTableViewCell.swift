83c16f0a7ab3920a05c8ccc5a99769db5d7fa903
Coconut-axolotl <Jfern666@fiu.edu>
2022-03-23 14:25:08 -0700

    Adding my project

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by user213225 on 3/22/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+      
+    }
+
+}
